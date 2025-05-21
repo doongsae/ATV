d7e9aa0378410c10020329bf090ed5ee27b50b7b
doongsae <doongsae@gmail.com>
2025-05-21 18:20:01 +0000

    Fix code

--- scripts/prepare_dataset.sh
+++ scripts/prepare_dataset.sh
@@ -1,5 +1,7 @@
 datasets=( "arc_challenge" "bbh" "bbq" "boolq" "crows_pairs" "commonsense_qa" "ethics" "glue_sst2" "hellaswag" "math_qa" "mmlu_pro" "openbookqa" "superglue_rte" "superglue_wic" "glue_mnli" "glue_qnli" "glue_cola" "deepmind" "mmlu" )
 
+mkdir -p dataset_files/abstractive
+
 for task in "${datasets[@]}"; do
     echo "Process ${task}"
     python process_data.py --task $task
